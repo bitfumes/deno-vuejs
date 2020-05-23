aff0ee6cbfd082e7e69a642b7adc7c346ae7040e
sarthaksavvy <bitfumes@gmail.com>
2020-05-23 23:46:29 +0530

    tailwindcss installed in vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div>
+    <div class="p-2 text-white bg-pink-500">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
