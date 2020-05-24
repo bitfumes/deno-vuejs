14d3df3d25382b5d8de6a5007b9d8f1bbef711f9
sarthaksavvy <bitfumes@gmail.com>
2020-05-24 21:16:35 +0530

    accessing protected routes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>
-    I am home
+  <div class="text-center">
+    <h1 class="text-3xl">Welcome, {{user.name}}</h1>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      user: {}
+    };
+  },
+  created() {
+    const token = this.$cookies.get("token");
+    axios.defaults.headers.Authorization = `Bearer ${token}`;
+    axios.get("http://localhost:4000/me").then(res => {
+      this.user = res.data;
+    });
+  }
+};
 </script>
 
 <style></style>
