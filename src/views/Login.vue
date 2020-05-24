92ca28f573cbb4616d3c7a7fd853f57cd5b6bf55
sarthaksavvy <bitfumes@gmail.com>
2020-05-24 11:15:40 +0530

    handled error and show them

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,8 +2,9 @@
   <div class="p-3 flex justify-center">
     <div class="w-1/2 py-10 flex flex-wrap justify-center border rounded">
       <h1 class="text-3xl w-full text-center">Login Page</h1>
-      <span v-if="errors.error" class="text-xs text-red-600">{{
-        errors.error
+      
+      <span v-if="errors.message" class="text-xs text-red-600">{{
+        errors.message
       }}</span>
       <form class="flex flex-wrap justify-center p-2" @submit.prevent="submit">
         <div class="py-2 w-full">
@@ -14,6 +15,9 @@
             class="p-2 w-full rounded border shadow"
           />
         </div>
+        <span v-if="errors[0]" class="text-xs text-red-600">{{
+        errors[0].email
+      }}</span>
         <div class="py-2 w-full">
           <input
             type="password"
@@ -22,6 +26,9 @@
             class="p-2 w-full rounded border shadow"
           />
         </div>
+        <span v-if="errors[1]" class="text-xs text-red-600">{{
+        errors[1].password
+      }}</span>
 
         <div class="py-2 w-full">
           <input
@@ -50,8 +57,10 @@ export default {
   },
   methods: {
     submit() {
-      axios.post("http://localhost:4000/login", this.form).catch((e) => {
-        this.errors = e.response.data;
+      this.errors = {}
+      axios.post("http://localhost:4000/login", this.form)
+      .catch((e) => {
+        this.errors = e.response.data.errors;
       });
     },
   },
